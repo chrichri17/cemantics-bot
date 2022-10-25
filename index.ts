e96a78e85ab34e852a249ca9374e01fd9759a1ed
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2022-10-25 15:02:21 +0200

    implement the bone of the app

--- index.ts
+++ index.ts
@@ -1,7 +1,40 @@
-import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.119.0/http/server.js";
 
-async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+const wordToGuess = "créteil";
+
+const baseUrl = "https://nlp.polytechnique.fr";
+
+async function getSimilarityScore(word: string): Promise<number> {
+    const body = {
+        sim1: wordToGuess,
+        sim2: word,
+        lang: "fr",
+        type: "General Word2Vec",
+    };
+    const response = await fetch(baseUrl + "/similarityscore", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+    const { simscore } = await response.json();
+    return Number(simscore);
+}
+
+async function extractGuess(req: Request): Promise<string> {
+    const payload = await req.formData();
+    const guess = payload.get("text")?.toString();
+    if (!guess) {
+        throw new Error("Guess is empty or null");
+    }
+    return guess;
+}
+
+async function handler(req: Request): Promise<Response> {
+    const guess = await extractGuess(req);
+    const score = await getSimilarityScore(guess);
+    return new Response(`Your guess score is : ${score.toFixed(2)}`);
 }
 
 serve(handler);
